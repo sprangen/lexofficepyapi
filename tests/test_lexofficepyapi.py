d4fd5ce66a57a9503225a046472f2b5f41d322d4
Maximilian Kindshofer <m.kindshofer@eu-con.net>
2020-12-01 10:46:50 +0100

    tests(contacts): Tests for Company Roles

--- tests/test_lexofficepyapi.py
+++ tests/test_lexofficepyapi.py
@@ -27,11 +27,69 @@ def lexoffice():
     api_key = os.environ.get("api_key")
     return Lexoffice(api_key=api_key)
 
+@pytest.fixture
+def company(lexoffice):
+    return lexoffice.create_company(name="Testfirma")
+
+def test_raise_if_create_company_has_no_name(lexoffice):
+    try:
+        lexoffice.create_company()
+    except ImproperlyConfigured:
+        pass
+    else:
+        raise
+
+def test_raise_if_create_company_has_a_bad_role(lexoffice):
+    try:
+        lexoffice.create_company(name="Fail", role="Fail")
+    except ImproperlyConfigured:
+        pass
+    else:
+        raise
+
+
+def test_raise_if_create_company_has_a_bad_roles_list(lexoffice):
+    try:
+        lexoffice.create_company(name="Fail", roles="Fail")
+    except ImproperlyConfigured:
+        pass
+    else:
+        raise
+
+def test_raise_if_create_company_has_a_bad_roles_list_entries(lexoffice):
+    try:
+        lexoffice.create_company(name="Fail", roles=["fail", "masterfail"])
+    except ImproperlyConfigured:
+        pass
+    else:
+        raise
+
+def test_raise_if_create_company_works_with_roles_list_entries(lexoffice):
+
+    company = lexoffice.create_company(name="TestCase", roles=["customer", "vendor"])
+
+    roles = company.get("roles")
+    assert roles.get("customer", False)
+    assert roles.get("vendor", False)
+
 
 def test_create_company(lexoffice):
     company = lexoffice.create_company(name="Testfirma")
 
     assert company.get("company").get("name") == "Testfirma"
+
+def test_raise_if_contact_has_no_id(lexoffice):
+    try:
+        lexoffice.get_contact()
+    except ImproperlyConfigured:
+        pass
+    else:
+        raise
+
+def test_retrieve_contact(company, lexoffice):
+    contact_id = company.get('id')
+    contact =lexoffice.get_contact(contact_id)
+    assert contact_id == contact.get('id')
 
 @pytest.fixture
 def response():
